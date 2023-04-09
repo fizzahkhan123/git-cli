7659e1980d67e40244be7330ad25e8e3bdfb2da9
fizzahkhan123 <guysr92@gmail.com>
2023-04-09 22:19:39 +0500

    FEAT: Initialize Project

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+// 3-april-2023
+
+// alert("hello world");
+// document.write("<h1>hello</h1>");
+
+// console.log(123);
+
+
+// var name = "ali";
+// var age = 24;
+// console.log(name);
+
+
+// alert("hello " +name + age);
+
+
+// let data = "Fizzah";
+// let data2 = " khan";
+
+// let data3 = data + data2;
+// console.log(data3);
+
+// let data = 5;
+// let data2 = 5;
+
+// let data3 = data + data2; // addition
+// console.log(data3);
+
+
+// let userName = prompt("what is your name?")
+// // console.log(userName);
+// document.write("Welcome to my website "+userName)
